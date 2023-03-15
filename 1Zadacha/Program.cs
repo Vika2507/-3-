c1deb9a9e5cf274359821a6ae4ec0feed26ad997
Viktoria <vikusya25072000@gmail.com>
2023-03-15 12:49:33 +0300

    добавила первую задачу

---
+++ 1Zadacha/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+string n = Console.ReadLine();
+int len = n.Length;
+
+if (len == 5)
+{
+    if (n[0] == n[4] && n[1] == n[3])
+    {
+        Console.WriteLine($"{n} палиндом");
+    }
+    else
+    {
+        Console.WriteLine($"не палиндом");
+    }
+}
+else 
+{
+    Console.WriteLine("Вы введи не пятизначное число");
+}
